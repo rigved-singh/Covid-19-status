2b877a124ebb47a91252f94bc447cd0272d7f89f
rigved-singh <103955732+rigved-singh@users.noreply.github.com>
2025-03-10 02:28:04 +0530

    handling api error
    Drop down list wasn't getting populated due to api call failing so had to handle the case

--- script.js
+++ script.js
@@ -2,53 +2,41 @@ $(document).ready(function(){
     init();
 
     function init() {
-        let url = "https://api.covid19api.com/summary"
-
-        let data = ""
+        let url = "https://api.covid19api.com/summary";
 
         $.get(url, function(data){
-
-            // adding options in the dropdown menu
-            let idx = 0;
             let Countries = data.Countries;
-            (Countries).forEach(element => {
-                $('#country').append(`<option value="${idx++}"> ${element.Country} </option>`);
-                // console.log((idx++)+ " " + element.Country);
+            $('#country').append(`<option value="global">Global</option>`);
+
+            Countries.forEach((element, index) => {
+                $('#country').append(`<option value="${index}">${element.Country}</option>`);
             });
 
-            // displaying datas in cards
-            $('#newCases').html(data.Global.NewConfirmed);
-            $('#totalCases').html(data.Global.TotalConfirmed);
+            // Display global data initially
+            updateCards(data.Global);
 
-            $('#newRecovered').html(data.Global.NewRecovered);
-            $('#recoveredCases').html(data.Global.TotalRecovered);
-
-            let activeCases = data.Global.TotalConfirmed - data.Global.TotalRecovered - data.Global.TotalDeaths;
-            $('#activeCases').html(activeCases);
-
-            $('#newDeaths').html(data.Global.NewDeaths);
-            $('#totalDeaths').html(data.Global.TotalDeaths);
-
-            console.log(Countries);
-
-            // get data for the country selected
+            // Change data based on country selection
             $("#country").change(function(){
                 let countryIdx = $("#country").val();
-                // console.log(Countries[countryIdx]);
-
-                // displaying datas in cards
-                $('#newCases').html(Countries[countryIdx].NewConfirmed);
-                $('#totalCases').html(Countries[countryIdx].TotalConfirmed);
-
-                $('#newRecovered').html(Countries[countryIdx].NewRecovered);
-                $('#recoveredCases').html(Countries[countryIdx].TotalRecovered);
-
-                activeCases = Countries[countryIdx].TotalConfirmed - Countries[countryIdx].TotalRecovered -Countries[countryIdx].TotalDeaths;
-                $('#activeCases').html(activeCases);
-
-                $('#newDeaths').html(Countries[countryIdx].NewDeaths);
-                $('#totalDeaths').html(Countries[countryIdx].TotalDeaths);
+                if (countryIdx === "global") {
+                    updateCards(data.Global);
+                } else {
+                    updateCards(Countries[countryIdx]);
+                }
             });
+
+        }).fail(function() {
+            alert("Failed to fetch data. Please try again later.");
         });
+
+        function updateCards(data) {
+            $('#newCases').html(data.NewConfirmed);
+            $('#totalCases').html(data.TotalConfirmed);
+            $('#newRecovered').html(data.NewRecovered);
+            $('#recoveredCases').html(data.TotalRecovered);
+            $('#activeCases').html(data.TotalConfirmed - data.TotalRecovered - data.TotalDeaths);
+            $('#newDeaths').html(data.NewDeaths);
+            $('#totalDeaths').html(data.TotalDeaths);
+        }
     }
 });
